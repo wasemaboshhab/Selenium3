63c55f18694726f61086a7d3f4cec45e417d9fd0
Wasim <wasem20aboshhab@gmail.com>
2021-07-28 21:10:50 +0300

    Course Elements Found - [(d-Flex) --> element] Need to filter out unnecessary elements!!

---
+++ src/main/java/Deff.java
@@ -0,0 +1,6 @@
+public class Deff {
+
+    public static final int FIRST_COURSE_ELEMENT = 65;
+    public static final int LAST_COURSE_ELEMENT = 65;
+
+}
--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -1,17 +1,22 @@
-import java.util.List;
+import java.sql.ClientInfoStatus;
+import java.sql.SQLOutput;
+import java.util.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.lang.model.util.Elements;
 import java.util.List;
 
 public class Selenium {
     public static void main(String[] args) {
 
-//                Scanner scanner = new Scanner(System.in);
-//        System.out.println("enter your userName :");
-//        String username = scanner.next();
-//        System.out.println("enter your password\n> :");
-//        String password = scanner.next();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your userName :");
+        String username = scanner.next();
+        System.out.println("enter your password\n> :");
+        String password = scanner.next();
 
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\user\\Desktop\\webdriver\\chromedriver.exe");
@@ -23,39 +28,64 @@ public class Selenium {
         List<WebElement> menuItems = menu.findElements(By.tagName("li"));
         WebElement personalInfo = menuItems.get(19);
 
+        personalInfo.click();
+        WebElement userNameInput = driver.findElement(By.id("Ecom_User_ID"));
+        WebElement userPasswordInput = driver.findElement(By.id("Ecom_Password"));
+        userNameInput.sendKeys(username); // <-- real username
+        userPasswordInput.sendKeys(password); // <- password
+        WebElement enter = driver.findElement(By.id("wp-submit"));
         try {
-            Thread.sleep(1000);
-            personalInfo.click();
-            WebElement userNameInput = driver.findElement(By.id("Ecom_User_ID"));
-            WebElement userPassword = driver.findElement(By.id("Ecom_Password"));
-            userNameInput.sendKeys("userName"); // <-- real username
-            userPassword.sendKeys("Password"); // <- password
-            WebElement enter = driver.findElement(By.id("wp-submit"));
-            Thread.sleep(1000);
+            Thread.sleep(500);
             enter.click();
             List<WebElement> personalInfo_Options = driver.findElements(By.className("row"));
             WebElement modelRow = personalInfo_Options.get(5);
             WebElement modelButton = modelRow.findElement(By.tagName("a"));
-                    String modelUrl = modelButton.getAttribute("href");
-//                    System.out.println(modelUrl);
-            Thread.sleep(1000);
+            Thread.sleep(500);
             modelButton.click();
+        //fix
+            WebElement container = driver.findElement(By.id("region-main"));
+            List<WebElement> menuShow = container.findElements(By.className("d-sm-inline-block"));
+            Thread.sleep(500);
+            menuShow.get(2).click();
+            Thread.sleep(500);
+            List<WebElement> styleOptions =container.findElements(By.tagName("a"));
+            styleOptions.get(12).click();
 
-                driver.get(modelUrl);
 
 
-//                here we will try to found the courses name
-            List<WebElement> courses = driver.findElements(By.className("multiline"));
-            if (courses != null) {
-                System.out.println(courses.size());
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                 /*//COURSES_ELEMENTS*/
+
+            List<WebElement> box = driver.findElements(By.className("d-flex"));
+
+//              *********************
+           System.out.println(box.size());
+            for (int i = 0; i < box.size(); i++) {
+            System.out.println("--------------------------------------------------------------------------------------");
+                System.out.println("\n" + i);
+                System.out.println(box.get(i).getText());
             }
 
-
-
-
-        } catch (InterruptedException e) {
-
+        } catch (Exception e) {
+            System.out.println("Exception...!!");
         }
 
     }
